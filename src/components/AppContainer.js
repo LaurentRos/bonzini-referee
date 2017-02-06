47e907042d03feaffd41306a36831010fdbe3ac3
ThierryCols <thierrycolsenet@gmail.com>
2017-02-06 22:54:05 +0100

    :wrench: startup react webpack config

---
+++ src/components/AppContainer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class AppContainer extends React.Component {
+  render() {
+    return (<div className="app-container">
+      <h1>Bonzini Referee</h1>
+    </div>);
+  }
+}
+
+export default AppContainer;
---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+var webpack = require('webpack');
+
+var config = {
+  entry: [
+    'webpack-dev-server/client?http://localhost:8080',
+    'webpack/hot/only-dev-server',
+    './src/index.js'
+  ],
+  output: {
+    path: './dist',
+    filename: 'bonzini-referee.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        loaders: ['babel']
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['', '.js'],
+  },
+  devServer: {
+    contentBase: './dist'
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+  ],
+}
+
+module.exports = config;
