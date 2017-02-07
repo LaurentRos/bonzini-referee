dba6057e23e472e0e46235f74dcd8cc5d49ab1a5
ThierryCols <thierrycolsenet@gmail.com>
2017-02-07 08:00:48 +0100

    :fireworks: add half feature

--- src/GoalStore.js
+++ src/GoalStore.js
@@ -1,26 +1,28 @@
 import mobx, { observable, computed } from 'mobx';
 
 class GoalStore {
-	@observable store = {
+  @observable store = {
     blue: {
-			goals: 0,
+  goals: 0,
       inOuts: 0
     },
     red: {
       goals: 0,
-			inOuts: 0
+  inOuts: 0
     },
-		teams: ['blue', 'red']
+  teams: ['blue', 'red'],
+  enableHalf: false,
+  half: 0
   };
 
-	constructor() {
-		mobx.autorun(() => console.log(this.score));
-	}
+  constructor() {
+  mobx.autorun(() => console.log(this.score));
+  }
 
-	@computed get score() {
+  @computed get score() {
     return `Blue ${this.store.blue.goals - this.store.red.inOuts} - ` +
       `${this.store.red.goals - this.store.blue.inOuts} Red`;
-	}
+  }
 }
 
 const goalStore = new GoalStore();
--- src/components/Commands.js
+++ src/components/Commands.js
@@ -6,14 +6,30 @@ import goalStore from '../GoalStore'
 
 @observer
 class Commands extends React.Component {
+
+  scoreGoal(team) {
+    goalStore.store[team].goals ++
+    goalStore.store[team].goals += goalStore.store.half
+    goalStore.store.half = 0
+  }
+
   render() {
 
     return (<div className="commands">
+      <label className="toggle-half">
+        <input
+          type="checkbox"
+          checked={ goalStore.store.enableHalf }
+          onClick={ () => goalStore.store.enableHalf = !goalStore.store.enableHalf }
+        >
+        </input>
+        Enable Half
+      </label>
       { goalStore.store.teams.map((team, idx) => {
         return(
           <Button
             key={idx}
-            action={ () => goalStore.store[team].goals ++ }
+            action={ () => this.scoreGoal(team) }
             label={`Goal ${team}`}
           />
         )
@@ -25,6 +41,11 @@ class Commands extends React.Component {
           </button>
         )
       }) }
+      {goalStore.store.enableHalf &&
+        <button onClick= { () => goalStore.store.half ++ }>
+          Score Half
+        </button>
+      }
     </div>)
   }
 }
